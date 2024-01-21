6e0d007dd7319c01446032fe7b527a3e57882734
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-01-21 09:29:43 +0700

    Initial Commit

---
+++ NavigationBottom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationBottom
+//
+//  Created by irfan wahendra on 21/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationBottom/NavigationBottomApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBottomApp.swift
+//  NavigationBottom
+//
+//  Created by irfan wahendra on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBottomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
